3a93374d0e4246fcbe1c29e9957acfec22a1413d
Roman Bulhakau <bulgakovra.w@gmail.com>
2023-04-20 17:03:31 +0300

    0007 A lot of different fixes: Allow null value in models. Add initialization of settings (empty)

--- customers/views/__init__.py
+++ customers/views/__init__.py
@@ -1,9 +1,11 @@
 from .customer_settings_view import CustomerSettingsViewSet
 from .customer_addresses_view import CustomerAddressesView
 from .customer_payments_view import CustomerPaymentsView
+from .customer_settings_init import CustomerSettingsInitView
 
 __all__ = (
     "CustomerSettingsViewSet",
     "CustomerAddressesView",
     "CustomerPaymentsView",
+    "CustomerSettingsInitView",
 )
---
+++ customers/views/customer_settings_init.py
@@ -0,0 +1,16 @@
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
+from customers.models import CustomerAddress, CustomerPaymentCardMethod, CustomerDictSettings
+
+
+class CustomerSettingsInitView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        user = self.request.user
+        settings = CustomerDictSettings.objects.create(customer=user)
+        CustomerAddress.objects.create(customer_settings=settings)
+        CustomerPaymentCardMethod.objects.create(customer_settings=settings)
+        return Response(status=HTTP_201_CREATED)
